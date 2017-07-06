88193abbe1fa46d38a949713235bb6a80bd4d72a
Carlos Garcia <cgarcia@thinkingcircles.com>
2017-07-05 22:57:15 -0400

    Package initial setup

---
+++ src/FlareDNSClientServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ThinkingCircles\FlareDNSClient;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
+
+
+class FlareDNSClientServiceProvider extends ServiceProvider
+{
+    protected $packageName = 'flaredns-client';
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        // Publish config
+        $this->publishes([
+            __DIR__.'/config/config.php' => config_path($this->packageName.'.php'),
+        ], 'config');
+
+        // Register Command
+        $this->commands([
+            FlareDNSClientSyncCommand::class
+        ]);
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom( __DIR__.'/config/config.php', $this->packageName);
+
+
+    }
+   
+
+}
---
+++ src/config/config.php
@@ -0,0 +1,12 @@
+<?php return [
+    
+    'cloudflare_api_account' => [
+        'cloudflare_zone_id' => null,
+        'cloudflare_global_api_key' => null,
+        'cloudflare_api_email' => null,
+        'dns_records' => [
+            ['id'=>null, 'name'=>null]
+        ]
+    ]
+
+];
