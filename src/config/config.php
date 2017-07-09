d5dbb7d07e371adae865d6352b499afe48412335
Carlos Garcia <cgarcia@thinkingcircles.com>
2017-07-08 22:28:03 -0400

    Documantation Updates

--- src/config/config.php
+++ src/config/config.php
@@ -1,12 +1,18 @@
 <?php return [
     
     'cloudflare_api_account' => [
-        'cloudflare_zone_id' => env('cloudflare_zone_id', null),
-        'cloudflare_global_api_key' => env('cloudflare_global_api_key', null),
-        'cloudflare_api_email' => env('cloudflare_api_email', null),
+
+        'cloudflare_zone_id' => env('cloudflare_zone_id', null), //ZONE ID
+
+        'cloudflare_global_api_key' => env('cloudflare_global_api_key', null), // API KEY
+
+        'cloudflare_api_email' => env('cloudflare_api_email', null), // API EMAIL
+
         'dns_records' => [
-            ['id'=>null, 'name'=>null]
+        	//Will match CloudFlare Record ID or domain name 
+            ['id'=>null, 'name'=>'wwww.domain.net'],
         ]
     ]
 
 ];
+
